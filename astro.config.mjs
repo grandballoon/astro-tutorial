0aabf31f5651fba27e823f85b81813699eaa93c0
Luke Conley <31871100+grandballoon@users.noreply.github.com>
2025-10-08 12:40:13 -0400

    completed non-JS portion of Astro tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
+import svelte from "@astrojs/svelte";
+
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://astrotutoriallsc.netlify.app",
+  integrations: [preact({ devtools: true }), svelte()],
+  adapter: netlify(),
+});
